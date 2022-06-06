1c5bc13050390eac497f74a99f512c8b89ef6ad9
mkoem <106806703+mkoem@users.noreply.github.com>
2022-06-06 19:39:39 +0300

    Assignment 3 . Practicing Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Assignment3Component } from './assignment3/assignment3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Assignment3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
