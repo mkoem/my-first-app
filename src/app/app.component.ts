df902613531b6ccb30a441c653874b06711bbab0
mkoem <106806703+mkoem@users.noreply.github.com>
2022-06-22 01:04:22 +0300

    Section 15-2 reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbidenUsernames= ['Chris','Anna'];
 
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        'username': new FormControl(null, [Validators.required,this.forbidenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('male', Validators.required),
+      'hobbies': new FormArray([])
+    })
+  };
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null,Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  forbidenNames(control: FormControl): {[s: string] : boolean} {
+    if(this.forbidenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbiden': true}
+    }
+    return null;
+  }
 }
+
+
+
+
